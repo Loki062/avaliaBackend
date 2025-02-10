8d9f5b2e001e51e9a41c7a94391a068d1b1d08af
LokiGoiano <lorddavi19@gmail.com>
2025-02-10 14:16:12 -0300

    api

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
   app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Agendamentos API!");
+  res.send("Welcome to the avaliações API!");
 });
 
   
